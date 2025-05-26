6549b0e4ed55960768bc8ef84ac2c3b4b7491c75
rishabhy043 <rishabh.knipss043@gmail.com>
2025-05-27 01:10:43 +0530

    commit Fourth

--- src/Revise.jsx
+++ src/Revise.jsx
@@ -1,42 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const EventProps = () => {
-  const buttonStyle = {
-    background: 'linear-gradient(135deg, #6e8efb, #a777e3)',
-    color: 'white',
-    padding: '14px 28px',
-    fontSize: '16px',
-    fontWeight: '600',
-    border: 'none',
-    borderRadius: '12px',
-    cursor: 'pointer',
-    boxShadow: '0 4px 15px rgba(0, 0, 0, 0.2)',
-    transition: 'all 0.3s ease-in-out',
-  };
-
-  const handleClick = (user) => {
-    alert(`Say hi to ${user}`);
-  };
-
-  const handleHover = () => {
-    alert(`Say hi to everyone from anywhere`);
-  };
-
-  const sayWelcome = () => {
-    alert('GOOD MORNING');
-  };
-
-  return (
+export const States=()=>{
+// let value=0;
+const[count, setCount]=useState(0);
+const incvalue=()=>{
+    // value++;
+    setCount(count+1);
+    // console.log(value);
+    console.log(count);
+}
+return(
     <>
-      <button style={buttonStyle} onClick={() => handleClick('Aman')}>
-        HandleClick
-      </button>
-      <button style={buttonStyle} onMouseEnter={handleHover}>
-        HandleHover
-      </button>
-      <button style={buttonStyle} onClick={sayWelcome}>
-        GREET
-      </button>
+    {/* <h1>{value}</h1> */}
+    <h1>{count}</h1>
+   <button onClick={incvalue}>INCREMENT</button>
     </>
-  );
-};
+)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './App.css';
-import { EventProps} from './Revise';
+import { States} from './Revise';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 {/* <HandleEvent></HandleEvent> */}
-<EventProps></EventProps>
+<States></States>
   </StrictMode>
 );
