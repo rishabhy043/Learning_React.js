0701ef900cab441d309134f76421e54d37d7b0b5
rishabhy043 <rishabh.knipss043@gmail.com>
2025-05-27 02:35:43 +0530

    states Challenge Done

--- src/Revise.jsx
+++ src/Revise.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 export const States=()=>{
 // let value=0;
 const[count, setCount]=useState(0);
+console.log("PARENT COMPONENT RENDERED");
 const incvalue=()=>{
     // value++;
     setCount(count+1);
@@ -14,6 +15,15 @@ return(
     {/* <h1>{value}</h1> */}
     <h1>{count}</h1>
    <button onClick={incvalue}>INCREMENT</button>
+   <ChildComponent>CHILD COMPONENT</ChildComponent>
     </>
 )
+}
+function ChildComponent(){
+    console.log("child component rendered");
+    return(
+        <>
+        <h1>CHILD COMPONENT RENDERED</h1>
+        </>
+    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './App.css';
 import { States} from './Revise';
+import { AcessUser } from './StateChallenge';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 {/* <HandleEvent></HandleEvent> */}
-<States></States>
+{/* <States></States> */}
+<AcessUser></AcessUser>
   </StrictMode>
 );
