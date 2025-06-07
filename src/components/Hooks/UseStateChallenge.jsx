e1e260dac26f1fae68d3c9f60d50326cae36e3a7
rishabhy043 <rishabh.knipss043@gmail.com>
2025-06-07 21:38:29 +0530

    More functionality added

--- src/components/Hooks/UseStateChallenge.jsx
+++ src/components/Hooks/UseStateChallenge.jsx
@@ -2,24 +2,33 @@ import { useState } from "react"
 
 export const UseStateChallenge = () => {
     const [count, setcount] = useState(0);
+    const [step, setstep] = useState(0);
     const Incbtn = () => {
-        setcount(count + 1);
+        setcount(count + step);
     }
     const Decbtn = () => {
-        setcount(count - 1);
+        setcount(count - step);
     }
     const Resetbtn = () => {
         setcount(0);
+        setstep(0);
     }
     return (<>
         <h1>This is counter App</h1>
         <div className="input">
-            <div>{count}</div>
-            <input type="number" />
+            <div>count: {count}</div>
+            <label>
+                    STEPS: 
+                    <input 
+                        type="number" 
+                        value={step} 
+                        onChange={(e) => setstep(Number(e.target.value))} 
+                    />
+                </label>
         </div>
         <div>
-            <button onClick={Incbtn}>INCREMENT</button>
-            <button onClick={Decbtn}>DECREMENT</button>
+            <button onClick={Incbtn} disabled={count >= 100}>INCREMENT</button>
+            <button onClick={Decbtn} disabled={count <= 0}>DECREMENT</button>
             <button onClick={Resetbtn}>RESET</button>
         </div>
     </>)
