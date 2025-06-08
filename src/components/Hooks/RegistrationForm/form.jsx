3fedf7ee6acaff1f8518bc5b2abae1f3d47d8c9b
rishabhy043 <rishabh.knipss043@gmail.com>
2025-06-08 19:37:17 +0530

    Registration for by RIGHT way

--- src/components/Hooks/RegistrationForm/form.jsx
+++ src/components/Hooks/RegistrationForm/form.jsx
@@ -12,147 +12,147 @@ import "./form.css";
 import { useState } from "react";
 
 export const RegistrationForm = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
 
-    switch (name) {
-      case "firstName":
-        setFirstName(value);
-        break;
+        switch (name) {
+            case "firstName":
+                setFirstName(value);
+                break;
 
-      case "lastName":
-        setLastName(value);
-        break;
+            case "lastName":
+                setLastName(value);
+                break;
 
-      case "email":
-        setEmail(value);
-        break;
+            case "email":
+                setEmail(value);
+                break;
 
-      case "password":
-        setPassword(value);
-        break;
+            case "password":
+                setPassword(value);
+                break;
 
-      case "phone":
-        setPhoneNumber(value);
-        break;
-    }
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    const formData = {
-      firstName,
-      lastName,
-      email,
-      password,
-      phoneNumber,
+            case "phone":
+                setPhoneNumber(value);
+                break;
+        }
     };
 
-    console.log(formData);
-  };
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const formData = {
+            firstName,
+            lastName,
+            email,
+            password,
+            phoneNumber,
+        };
 
-  return (
-    <>
-      <form onSubmit={handleFormSubmit}>
-        <div className="container">
-          <h1>Sign Up</h1>
-          <p>Please fill in this form to create an account.</p>
+        console.log(formData);
+    };
 
-          <label htmlFor="firstName">
-            <b>First Name</b>
-          </label>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="Enter firstName"
-            required
-            value={firstName}
-            onChange={handleInputChange}
-          />
+    return (
+        <>
+            <form onSubmit={handleFormSubmit}>
+                <div className="container">
+                    <h1>Sign Up</h1>
+                    <p>Please fill in this form to create an account.</p>
 
-          <label htmlFor="lastName">
-            <b>Last Name</b>
-          </label>
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Enter lastName"
-            required
-            value={lastName}
-            onChange={handleInputChange}
-          />
+                    <label htmlFor="firstName">
+                        <b>First Name</b>
+                    </label>
+                    <input
+                        type="text"
+                        name="firstName"
+                        placeholder="Enter firstName"
+                        required
+                        value={firstName}
+                        onChange={handleInputChange}
+                    />
 
-          <label htmlFor="email">
-            <b>Email</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Email"
-            name="email"
-            required
-            value={email}
-            onChange={handleInputChange}
-          />
+                    <label htmlFor="lastName">
+                        <b>Last Name</b>
+                    </label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        placeholder="Enter lastName"
+                        required
+                        value={lastName}
+                        onChange={handleInputChange}
+                    />
 
-          <label htmlFor="password">
-            <b>Password</b>
-          </label>
-          <input
-            type="password"
-            placeholder="Enter Password"
-            name="password"
-            required
-            value={password}
-            onChange={handleInputChange}
-          />
+                    <label htmlFor="email">
+                        <b>Email</b>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Email"
+                        name="email"
+                        required
+                        value={email}
+                        onChange={handleInputChange}
+                    />
 
-          <label htmlFor="phone">
-            <b>Phone Number</b>
-          </label>
+                    <label htmlFor="password">
+                        <b>Password</b>
+                    </label>
+                    <input
+                        type="password"
+                        placeholder="Enter Password"
+                        name="password"
+                        required
+                        value={password}
+                        onChange={handleInputChange}
+                    />
 
-          <input
-            type="phone"
-            name="phone"
-            placeholder="9876543211"
-            required
-            value={phoneNumber}
-            onChange={handleInputChange}
-          />
+                    <label htmlFor="phone">
+                        <b>Phone Number</b>
+                    </label>
 
-          <p>
-            By creating an account you agree to our
-            <a href="#" style={{ color: "dodgerblue" }}>
-              Terms & Privacy
-            </a>
-          </p>
+                    <input
+                        type="phone"
+                        name="phone"
+                        placeholder="9876543211"
+                        required
+                        value={phoneNumber}
+                        onChange={handleInputChange}
+                    />
 
-          <div className="clearfix">
-            <button type="submit" className="btn">
-              Sign Up
-            </button>
-          </div>
-        </div>
-      </form>
+                    <p>
+                        By creating an account you agree to our
+                        <a href="#" style={{ color: "dodgerblue" }}>
+                            Terms & Privacy
+                        </a>
+                    </p>
 
-      <section
-        className="summary"
-        style={{ textAlign: "center", marginTop: "30px" }}
-      >
-        <p>
-          Hello, my name is
-          <span>
-            {firstName} {lastName}
-          </span>
-          . My email address is <span>{email}</span> and my phone number is
-          <span>{phoneNumber}</span>.
-        </p>
-      </section>
-    </>
-  );
+                    <div className="clearfix">
+                        <button type="submit" className="btn">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            <section
+                className="summary"
+                style={{ textAlign: "center", marginTop: "30px" }}
+            >
+                <p>
+                    Hello, my name is
+                    <span>
+                        {firstName} {lastName}
+                    </span>
+                    . My email address is <span>{email}</span> and my phone number is
+                    <span>{phoneNumber}</span>.
+                </p>
+            </section>
+        </>
+    );
 };
