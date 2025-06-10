ea34d0d6ea16180524a0a574497339f02352f6df
rishabhy043 <rishabh.knipss043@gmail.com>
2025-06-10 17:50:08 +0530

    Started

--- src/components/ApiLearning/ApiNot.jsx
+++ src/components/ApiLearning/ApiNot.jsx
@@ -3,16 +3,16 @@ import { useState } from "react"
 export const HowNotToFetchDataApi = () => {
     const [apidata, setapidata] = useState([]);
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => res.json())
-        .then((data) => setapidata(data))
+    fetch("https://jsonplaceholder.typicode.com/posts")  //fetch() is called directly in the component body
+        .then((res) => res.json())                        //Why it's wrong: React components re-render often, 
+        .then((data) => setapidata(data))                 
         .catch((error) => console.log(error));
     return (<>
         <div className="container">
             <ul>
                 Data:  {
                     apidata.map((curData) => {
-                        return <li key={curData.id}>{curData.post}</li>
+                        return <li key={curData.id}>{curData.title}</li>
                     })
                 }
             </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './App.css';
-import { HowNotToFetchDataApi } from './components/ApiLearning/ApiNot';
+import { HowToFetchDataApi } from './components/ApiLearning/ApiNot';
 
 
 
 createRoot(document.getElementById('root')).render(
 <StrictMode>
-<HowNotToFetchDataApi></HowNotToFetchDataApi>
+<HowToFetchDataApi></HowToFetchDataApi>
 </StrictMode>
 );
