3f2abe3133dd4c0e84df1b9d84b87d9167f83693
rishabhy043 <rishabh.knipss043@gmail.com>
2025-06-18 00:55:20 +0530

    UnderProgress

--- ReactRouterProject/src/App.jsx
+++ ReactRouterProject/src/App.jsx
@@ -1,38 +1,46 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
- import {Home} from "./pages/Home"
-import {About} from "./pages/About"
-import {Contact} from "./pages/Contact"
-import {Movie} from "./pages/Movie"
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Contact } from "./pages/Contact"
+import { Movie } from "./pages/Movie"
+import { AppLayout } from './Components/layout/AppLayout'
 
 const App = () => {
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<Home></Home>
-  },
-  {
-    path:"/about",
-    element: <About></About>
-  },
-  {
-    path:"/movie",
-    element: <Movie></Movie>
-  },
-  {
-    path:"/contact",
-    element: <Contact></Contact>
-  },
-   {
-    path:"/home",
-    element: <Home></Home>
-  }
-])
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+
+        {
+          path: "/",
+          element: <Home></Home>
+        },
+        {
+          path: "/about",
+          element: <About></About>
+        },
+        {
+          path: "/movie",
+          element: <Movie></Movie>
+        },
+        {
+          path: "/contact",
+          element: <Contact></Contact>
+        },
+        {
+          path: "/home",
+          element: <Home></Home>
+        }
+      ],
+    }
+  ])
 
   return (<>
-  <h1>Hello React Router</h1>
-  <RouterProvider router={router}></RouterProvider>
+    <h1>Hello React Router</h1>
+    <RouterProvider router={router}></RouterProvider>
   </>
   )
 }
