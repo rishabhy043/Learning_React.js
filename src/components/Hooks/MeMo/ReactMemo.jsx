9a4824c29ad582d046790ff241816f0fd0f31699
rishabhy043 <rishabh.knipss043@gmail.com>
2025-06-17 11:27:11 +0530

    Memo Topic Done

--- src/components/Hooks/MeMo/ReactMemo.jsx
+++ src/components/Hooks/MeMo/ReactMemo.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import RenderCount from "./RenderCount"; // Corrected import
 
 export const ReactMemo = () => {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>{count}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>INCREMENT</button>
-      
-      {/* RenderCount won't re-render if memoization works */}
-      <RenderCount />
-    </>
-  );
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={() => setCount((prev) => prev + 1)}>INCREMENT</button>
+
+            {/* RenderCount won't re-render if memoization works */}
+            <RenderCount />
+        </>
+    );
 };
